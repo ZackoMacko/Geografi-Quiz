0038cd5bc90c2cab9c74f674c6879666702db2df
Isaac Nilsson <isaac.okrypterat@gmail.com>
2025-05-02 19:05:11 +0200

    Added variables and Methods
    Variables
    var answerGridItem0
    var answerGridItem1
    var answerGridItem2
    var answerGridItem3
    
    polyPicture0
    polyPicture1
    polyPicture2
    polyPicture3
    
    startButton
     restartButton
    
     images
     numbers
     type
     answerText
     answers
    correctanswer
    
    userOptions
    
    questionData;
    
    Methods:
    UserOptions()
    fetchQuestionData()

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
-var monopicturegriditem0 = document.getElementById("item0");
+/**
+ * Title: Geography question/answer handler
+ *
+ * The intention of this file is to handle the input from the user.
+ * Summarization of what the functions does:
+ * When the user picks a wrong answer it clearly indicates it and moves on to the next question.
+ * When the user picks a right answers i t clearly indicates it and moves on to the next question.
+ * Indicates when the Quiz is over. 
+ * Gives the user the choice to restart at the end.
+ */
+
+/**
+ * Variables for the choice boxes the user can choose between:
+ */
+var answerGridItem0 = document.getElementById("item0");
+var answerGridItem1 = document.getElementById("item1");
+var answerGridItem2 = document.getElementById("item2");
+var answerGridItem3 = document.getElementById("item3");
+
+/**
+ * Variables for the images for the poly-image-grid:
+ */
+var polyPicture0 = document.getElementById("questitem0");
+var polyPicture1 = document.getElementById("questitem1");
+var polyPicture2 = document.getElementById("questitem2");
+var polyPicture3 = document.getElementById("questitem3");
+
+/**
+ * Variable for the start-page-grid:
+ */
+var startButton= document.getElementById("startbutton");
+var restartButton = document.getElementById("restartbutton");
+
+let images = [];
+let numbers =[];
+let type=[];
+var answerText;
+let answers = [];
+var correctanswer;
+
+let userOptions= [];
+
+var questionData;
 
 
+function UserOptions() {
+    userOptions = [answerGridItem0,answerGridItem1,answerGridItem2,answerGridItem3];
+}
+
+function fetchQuestionData() {
+
+    fetch("./data/questions.json")
+    .then((response) =>  response.json())
+
+    .then((data) => {
+    questionData = data;   
+    return data
+    })
+
+    .catch(error => console.error('Failed to fetch data:', error)); 
+};
+
+
+
+function Main()
+{
+
+}
